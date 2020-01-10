4a68f0a2e23175da48121dcd8d3088a027b61808
9ingen <47188362+9ingen@users.noreply.github.com>
2020-01-10 15:01:25 +0530

    Updated Shader Class

--- GLExamples/src/Application_2.cpp
+++ GLExamples/src/Application_2.cpp
@@ -1,4 +1,4 @@
-/*#include "GL/glew.h"
+#include "GL/glew.h"
 #include "GLFW/glfw3.h"
 #include "shader.h"
 #include "stb_image.h"
@@ -57,7 +57,7 @@ int main()
 	Shader myShader("src/texture.vs", "src/texture.fs");
 
 	//Buffers
-	float vertices[] = 
+	float vertices[] =
 	{
 		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 		 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
@@ -100,6 +100,20 @@ int main()
 		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
 		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
 		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+	};
+
+	// world space positions of our cubes
+	glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
 	};
 
 	unsigned int VAO, VBO;
@@ -158,7 +172,7 @@ int main()
 	//set texture filtering parameters
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	
+
 	//load image, create texture, generate mipmaps
 	data = stbi_load("res/dio.png", &width, &height, &nrchannels, 0);
 	if (data)
@@ -187,34 +201,44 @@ int main()
 		// Render here 
 		glClearColor(0.0f, 0.0f, 0.25f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		
+
 		// Bind textures to corresponding texture units
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, texture1);
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, texture2);
 
+		//activate shader
+		myShader.Use();
+
 		// Create Transformations
-		glm::mat4 model = glm::mat4(1.0f); //identity matrix
 		glm::mat4 view = glm::mat4(1.0f);
 		glm::mat4 projection = glm::mat4(1.0f);
-		model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 1.0f));
 		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 		projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-
-		// Retrieve matrix uniform locations
-		unsigned int modelLoc = glGetUniformLocation(myShader.ID, "model");
+		//myShader.SetMat4("view", view);
+		//myShader.SetMat4("projection", projection);
 		unsigned int viewLoc = glGetUniformLocation(myShader.ID, "view");
 		unsigned int projLoc = glGetUniformLocation(myShader.ID, "projection");
 
 		// Pass them to shaders
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
 		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
-		// Render Triangle
+		// Render Boxes
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			//calculate model matrix for each object and pass it to shader before drawing
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * i;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.2f, 0.6f));
+			unsigned int modelLoc = glGetUniformLocation(myShader.ID, "model");
+			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+			//myShader.SetMat4("model", model);
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 
 		// Swap front and back buffers 
 		glfwSwapBuffers(window);
@@ -222,6 +246,8 @@ int main()
 		// Poll for and process events 
 		glfwPollEvents();
 	}
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
 
 	glfwTerminate();
 	return 0;
@@ -237,4 +263,3 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
 }
-*/
--- GLExamples/src/shader.h
+++ GLExamples/src/shader.h
@@ -3,6 +3,16 @@
 
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
+
+#include "glm/glm.hpp"
+#include "glm/vec3.hpp"
+#include "glm/vec4.hpp"
+#include "glm/mat4x4.hpp"
+#include "glm/ext/matrix_transform.hpp"
+#include "glm/ext/matrix_clip_space.hpp"
+#include "glm/ext/scalar_constants.hpp"
+#include "glm/gtc/type_ptr.hpp"
+
 #include <iostream>
 #include <string>
 
@@ -16,12 +26,18 @@ public:
 	
 	//Functions
 	void Use();
-	void SetBool(const std::string& name, bool value)const;
-	void SetInt(const std::string& name, int value)const;
-	void SetFloat(const std::string& name, float value)const;
-	void SetVec2(const std::string& name, float x, float y)const;
-	void SetVec3(const std::string& name, float x, float y, float z)const;
-	void SetVec4(const std::string& name, float x, float y, float z, float w)const;
+	void SetBool(const std::string& name, bool value) const;
+	void SetInt(const std::string& name, int value) const;
+	void SetFloat(const std::string& name, float value) const;
+	void SetVec2(const std::string &name, const glm::vec2 &value) const;
+	void SetVec2(const std::string& name, float x, float y) const;
+	void SetVec3(const std::string &name, const glm::vec3 &value) const;
+	void SetVec3(const std::string& name, float x, float y, float z) const;
+	void SetVec4(const std::string &name, const glm::vec4 &value) const;
+	void SetVec4(const std::string& name, float x, float y, float z, float w) const;
+	void SetMat2(const std::string &name, const glm::mat2 &mat) const;
+	void SetMat3(const std::string &name, const glm::mat3 &mat) const;
+	void SetMat4(const std::string &name, const glm::mat4 &mat) const;
 
 private:
 	//Utility Function
